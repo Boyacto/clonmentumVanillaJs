8ca2a34fc3270b90e9cfb7706a97d133050d7d7d
Boyacto <bohyeoncha2000@gmail.com>
2021-06-21 12:17:15 +0900

    clock.js bug fixed and randomQuote Added

---
+++ js/quotes.js
@@ -0,0 +1,50 @@
+const quotes = [
+    {
+        quote: "If you want to live a happy life, tie ti to a goal, not the people or things.",
+        author: "Albert Einstein",
+    },
+    {
+        quote: "Let me tell you the secret that has led me to my goal. My strength lies solely in my tenacity",
+        author: "Louis Pasteur",
+    },
+    {
+        quote: "A journey of a thousand miles must begin with a single step.",
+        author: "Lao Tzu",
+    },
+    {
+        quote: "The time won't wait you until you run next to them.",
+        author: "Unknown tomato",
+    },
+    {
+        quote: "Better three hours too soon than a minute too late.",
+        author: "William Shakespeare",
+    },
+    {
+        quote: "You can't change the volume of your time, but you can change the density.",
+        author: "Unknown tomato",
+    },
+    {
+        quote: "The key is in not spending time, but in investing it.",
+        author: "Stephen R. Covey",
+    },
+    {
+        quote: "Punctuality is the thief of time.",
+        author: "Oscar Wilde",
+    },
+    {
+        quote: "You can’t make up for lost time. You can only do better in the future.",
+        author: "Ashley Ormon",
+    },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+
+
+function ramdomQuoute(){
+    const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    quote.innerText = todaysQuote.quote;
+    author.innerText = todaysQuote.quote;
+
+}
